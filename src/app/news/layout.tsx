370c8cec6920f893aa79be8137a55731098c297d
shim <k-ohshima@shoeisha.co.jp>
2024-04-29 01:57:27 +0900

    6章まで終了

---
+++ src/app/news/layout.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/app/_components/Hero";
+import Sheet from "@/app/_components/Sheet";
+type Props = {
+  children: React.ReactNode;
+};
+export default function NewsLayout({ children }: Props) {
+  return (
+    <>
+      <Hero title="News" sub="ニュース" />
+      <Sheet>{children}</Sheet>
+    </>
+  );
+}
