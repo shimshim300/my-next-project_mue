f99779f4cf223a7a0330888d602a1c0428f8f13a
shim <k-ohshima@shoeisha.co.jp>
2024-04-29 14:00:05 +0900

    7章まで完了

--- src/app/news/category/[id]/p/[current]/page.tsx
+++ src/app/news/category/[id]/p/[current]/page.tsx
@@ -1,15 +1,14 @@
-import { getNewsList } from "@/app/_libs/microcms";
+import { notFound } from "next/navigation";
+
+import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
 import Pagination from "@/app/_components/Pagination";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
-import { notFound } from "next/navigation";
-import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
 
 type Props = {
   params: {
     id: string;
     current: string;
-    basePath={`/news/category/${category.id}`}
   };
 };
 
@@ -20,22 +19,27 @@ export default async function Page({ params }: Props) {
     notFound();
   }
 
-  const category = await getCategoryDetail(params.id).catch(notFound);
+  // params.idが不正な場合は404を返す
+  const category = await getCategoryDetail(params.id);
 
-  const { contents: articles, totalCount } = await getNewsList({
+  const { contents: news, totalCount } = await getNewsList({
     filters: `category[equals]${category.id}`,
     limit: NEWS_LIST_LIMIT,
     offset: NEWS_LIST_LIMIT * (current - 1),
   });
 
-  if (articles.length === 0) {
+  if (news.length === 0) {
     notFound();
   }
 
   return (
     <>
-      <NewsList articles={articles} />
-      <Pagination totalCount={totalCount} current={current} />
+      <NewsList news={news} />
+      <Pagination
+        totalCount={totalCount}
+        current={current}
+        basePath={`/news/category/${category.id}`}
+      />
     </>
   );
 }
